4d93deb6d7bd6810c56067f5c8e9cded22923069
Tim Campbell <timothylevicampbell@gmail.com>
2017-11-02 15:56:11 -0400

    Change password == to authenticate in post sessions

--- 12_sinatra_sessions/app/controllers/application_controller.rb
+++ 12_sinatra_sessions/app/controllers/application_controller.rb
@@ -14,7 +14,7 @@ class ApplicationController < Sinatra::Base
 
   post '/sessions' do
     user = User.find_by(username: params[:username])
-    if user.password == params[:password]
+    if user.authenticate(params[:password])
       # log the user in to my site
       session[:user_id] = user.id
       redirect "/books"
